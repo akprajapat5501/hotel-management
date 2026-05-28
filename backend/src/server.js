4f425641ad1088aeb9a0d5edcd3ad27dd58ffad4
AAKASH KUMAR <162182656+akprajapat5501@users.noreply.github.com>
2026-05-28 12:43:01 +0530

    Update CORS configuration to allow multiple origins

--- backend/src/server.js
+++ backend/src/server.js
@@ -14,12 +14,13 @@ const userProfile = require("./routes/userProfile");
 const app = express();
 const PORT = 9000;
 
-app.use(
-    cors({
-        origin: "http://localhost:5173",
-        credentials: true,
-    })
-);
+app.use(cors({
+  origin: [
+    "http://localhost:5173",
+    "https://hotel-management-4jq2.onrender.com"
+  ],
+  credentials: true
+}))
 
 app.use(express.json());
 app.use(cookieParser());
